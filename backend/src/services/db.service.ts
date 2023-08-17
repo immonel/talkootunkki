0a810486f11b1c79c1e12ab73b76e9695b1d6931
Leo Immonen <leo.j.immonen99@gmail.com>
2023-08-17 13:22:22 +0300

    Disable sequelize logging

--- backend/src/services/db.service.ts
+++ backend/src/services/db.service.ts
@@ -5,6 +5,7 @@ const PG_USERNAME = process.env.POSTGRES_USER     || '';
 
 export const sequelize = new Sequelize('talkoot', PG_USERNAME, PG_PASSWORD, {
     host:    'postgres',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging: false
   }
 )
