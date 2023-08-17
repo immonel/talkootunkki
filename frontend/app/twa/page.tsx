227e774c9f9efd0aebcb017747d5c504c25ec9d2
Leo Immonen <leo.j.immonen99@gmail.com>
2023-08-17 13:24:17 +0300

    Add loading state to TWA front page

--- frontend/app/twa/page.tsx
+++ frontend/app/twa/page.tsx
@@ -9,20 +9,30 @@ const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL || ''
 
 const Page = () => {
   const [ loggedIn, setLoggedIn ] = useState(false)
+  const [ loading, setLoading ] = useState(true)
   const initData = WebApp.initData
 
   useEffect(() => {
     WebApp.BackButton.hide()
 
     axios.post(`${baseUrl}/api/register/check`, { initData })
-      .then(ok => setLoggedIn(true))
-      .catch(error => setLoggedIn(false))
+      .then(ok => {
+        setLoggedIn(true)
+        setLoading(false)
+      })
+      .catch(error => {
+        setLoggedIn(false)
+        setLoading(false)
+      })
   }, [ initData ])
 
   return (
-    <>
-      { loggedIn ? <LoggedIn initData={initData} setLoggedIn={setLoggedIn} /> : <FrontPage /> }
-    </>
+    <div className='flex flex-col w-full mt-10 items-center'>
+      { 
+        !loading &&
+        (loggedIn ? <LoggedIn initData={initData} setLoggedIn={setLoggedIn} /> : <FrontPage />)
+      }
+    </div>
   )
 }
 
