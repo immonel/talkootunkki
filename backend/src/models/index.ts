d3d70d02411dcfa042b92cd9c3da3590a30fd2ed
Leo Immonen <leo.j.immonen99@gmail.com>
2023-08-24 14:54:11 +0300

    Fix eager loading with join table

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -12,6 +12,22 @@ Event.belongsToMany(Participant, { through: {
   unique: false
 }, foreignKey: 'event_id' })
 
+
+// Super Many-to-Many relationship, allows eager loading with join table
+// https://sequelize.org/docs/v6/advanced-association-concepts/advanced-many-to-many/#the-best-of-both-worlds-the-super-many-to-many-relationship
+Participant.hasMany(Participation, {
+  foreignKey: 'user_id'
+})
+Participation.belongsTo(Participant, {
+  foreignKey: 'user_id'
+})
+Event.hasMany(Participation, {
+  foreignKey: 'event_id'
+})
+Participation.belongsTo(Event, {
+  foreignKey: 'event_id'
+})
+
 Event.sync({
   // alter: true
 })
