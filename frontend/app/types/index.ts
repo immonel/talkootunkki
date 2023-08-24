f632ea3d20d8a097dc7936c5b4957e2a45874297
Leo Immonen <leo.j.immonen99@gmail.com>
2023-08-24 15:22:33 +0300

    Add type definitions

--- frontend/app/types/index.ts
+++ frontend/app/types/index.ts
@@ -5,7 +5,30 @@ export interface Event {
   end_date: string;
 }
 
+export interface Participant {
+  username: string | null;
+  first_name: string;
+  last_name: string | null;
+  email: string | null;
+}
+
+export interface Participation {
+  participation_id: string;
+  user_id: string;
+  start_date: string;
+  end_date: string;
+  association: string | null;
+}
+
 export type LeaderboardAssociation = {
   name: string;
   totalTime: number;
+}
+
+export type EventWithLeaderboards = {
+  event_id: string;
+  event_name: string;
+  start_date: string;
+  end_date: string;
+  leaderboards: LeaderboardAssociation[];
 }
