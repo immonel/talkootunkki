df26e42c7717ff5c18200800ac66fd4b91214262
Leo Immonen <leo.j.immonen99@gmail.com>
2023-08-29 22:04:26 +0300

    Add instructions for registering

--- frontend/src/pages/code/index.tsx
+++ frontend/src/pages/code/index.tsx
@@ -1,8 +1,35 @@
 import Code from "@components/common/Code";
 
+type HighlightProps = {
+  children: React.ReactNode
+}
+
+const Highlight = ({ children }: HighlightProps) => (
+  <span className="text-orange-400">{children}</span>
+)
+
 const CodePage = () => (
-  <main className="flex min-h-screen items-center justify-center overflow-x-hidden">
-    <Code />
+  <main className="flex flex-col min-h-screen items-center justify-center overflow-x-hidden">
+    <h1 className="text-3xl mb-12">Register to Village Cleanup</h1>
+    <div className="flex flex-col gap-5 items-center">
+      <p>1. Search for <Highlight>@TalkooBot</Highlight> in Telegram</p>
+      <p>2. Start the web app from the <Highlight>Open</Highlight> button</p>
+      <p>3. Press <Highlight>Register</Highlight> and follow the instructions</p>
+      <p>4. Scan this <Highlight>QR code</Highlight> to finish your registration!</p>
+    </div>
+    <div className="my-20">
+      <Code />
+    </div>
+    <div className="flex flex-col gap-1 w-1/3 text-center">
+      <small>
+        Disclaimer: Information about your Telegram account and contact information (if provided)
+        will be recorded to track your participation time and for contacting purposes.
+      </small>
+      <small>
+        <em>You can also help in cleaning the village without registering!</em>
+      </small>
+
+    </div>
   </main>
 )
 
