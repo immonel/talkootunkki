f6eda79f1584b07cb052ce4962e13921b9d7c6f0
Leo Immonen <leo.j.immonen99@gmail.com>
2026-04-24 00:56:25 +0300

    Switch from postgres to sqlite

--- backend/src/services/db.service.ts
+++ backend/src/services/db.service.ts
@@ -1,11 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-const PG_PASSWORD = process.env.POSTGRES_PASSWORD || '';
-const PG_USERNAME = process.env.POSTGRES_USER     || '';
-
-export const sequelize = new Sequelize('talkoot', PG_USERNAME, PG_PASSWORD, {
-    host:    'postgres',
-    dialect: 'postgres',
-    logging: false
-  }
-)
+export const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: '/data/talkoot.db',
+    logging: false,
+})
